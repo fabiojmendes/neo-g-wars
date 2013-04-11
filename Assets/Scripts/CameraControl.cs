4fb024b4490c56faf360075fe3c08ddb17260ad1
Fabio Mendes <fabiojmendes@gmail.com>
2013-04-11 03:10:02 -0300

    Camera

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -14,16 +14,8 @@ public class CameraControl : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
 		Vector3 wantedPoint = character.position + diff;
 		this.transform.position = Vector3.Lerp(this.transform.position, wantedPoint, Time.deltaTime * damping);
 	}
-
-	/*
-	void OnGUI() {
-		GUI.Label(new Rect (10, 10, Screen.width-10, Screen.height-10), "Char " + character.position.ToString());
-		GUI.Label(new Rect (10, 25, Screen.width-10, Screen.height-10), "Camera " + this.transform.position.ToString());
-		GUI.Label(new Rect (10, 40, Screen.width-10, Screen.height-10), "Diff " + diff.ToString());
-		GUI.Label(new Rect (10, 55, Screen.width-10, Screen.height-10), "Wanted " + (character.position + diff).ToString());
-	}*/
 }
