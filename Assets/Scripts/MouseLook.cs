1a8504a6b5d002d905e3f72a5da3997b3e021a11
Fabio Mendes <fabiojmendes@gmail.com>
2013-04-17 11:10:35 -0300

    Alteração dos scripts

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,24 +5,9 @@ public class MouseLook : MonoBehaviour {
 
 	Plane playerMovementPlane;
 	
-	Quaternion screenMovementSpace;
-	Vector3 screenMovementForward;
-	Vector3 screenMovementRight;
-	
-	//string debugOutput;
-	
 	void Awake() {
 		// caching movement plane
-		playerMovementPlane = new Plane (transform.up, transform.position);
-	}
-
-	void Start() {
-		screenMovementSpace = Quaternion.Euler (0, Camera.main.transform.eulerAngles.y, 0);
-		screenMovementForward = screenMovementSpace * Vector3.forward;
-		screenMovementRight = screenMovementSpace * Vector3.right;
-		Debug.Log("ScreenMvSpc: " + screenMovementSpace);
-		Debug.Log("ScreenMvFw: " + screenMovementForward);
-		Debug.Log("ScreenMvR: " + screenMovementRight);
+		playerMovementPlane = new Plane(transform.up, transform.position);
 	}
 
 	void Update() {
@@ -33,7 +18,7 @@ public class MouseLook : MonoBehaviour {
 		Vector3 cursorScreenPosition = Input.mousePosition;
 					
 		// Find out where the mouse ray intersects with the movement plane of the player
-		Vector3 cursorWorldPosition = ScreenPointToWorldPointOnPlane (cursorScreenPosition, playerMovementPlane, Camera.main);
+		Vector3 cursorWorldPosition = ScreenPointToWorldPointOnPlane(cursorScreenPosition, playerMovementPlane, Camera.main);
 		
 		float halfWidth = Screen.width / 2.0f;
 		float halfHeight = Screen.height / 2.0f;
@@ -44,18 +29,15 @@ public class MouseLook : MonoBehaviour {
 		posRel.x /= maxHalf; 
 		posRel.y /= maxHalf;
 					
-		cameraAdjustmentVector = posRel.x * screenMovementRight + posRel.y * screenMovementForward;
+		cameraAdjustmentVector = posRel.x * Vector3.right + posRel.y * Vector3.forward;
 		cameraAdjustmentVector.y = 0;	
 								
 		// The facing direction is the direction from the character to the cursor world position
 		Vector3 facingDirection = (cursorWorldPosition - transform.position);
 		facingDirection.y = 0;
 		
-		//transform.rotation = Quaternion.FromToRotation(transform.position, facingDirection);
-		
-		float rotationAngle = AngleAroundAxis (transform.forward, facingDirection, Vector3.up);
-		transform.Rotate(Vector3.up, rotationAngle);
-		
+		RotateAroundAxis(transform.forward, facingDirection);
+	
 		//debugOutput = facingDirection.ToString();
 	}
 	
@@ -70,25 +52,24 @@ public class MouseLook : MonoBehaviour {
 		Ray ray = camera.ScreenPointToRay(screenPoint);
 		
 		// Find out where the ray intersects with the plane
-		return PlaneRayIntersection (plane, ray);
+		return PlaneRayIntersection(plane, ray);
 	}
 	
-	// The angle between dirA and dirB around axis
-	float AngleAroundAxis (Vector3 dirA, Vector3 dirB, Vector3 axis) {
-	    // Project A and B onto the plane orthogonal target axis
-	    dirA = dirA - Vector3.Project(dirA, axis);
-	    dirB = dirB - Vector3.Project(dirB, axis);
-	   
+	// The angle between dirA and dirB around the up axis
+	void RotateAroundAxis(Vector3 dirA, Vector3 dirB) {
+		Vector3 axis = Vector3.up;
+		
 	    // Find (positive) angle between A and B
 	    float angle = Vector3.Angle(dirA, dirB);
 	   
 	    // Return angle multiplied with 1 or -1
-	    return angle * (Vector3.Dot(axis, Vector3.Cross(dirA, dirB)) < 0 ? -1 : 1);
+	    angle = angle * (Vector3.Dot(axis, Vector3.Cross(dirA, dirB)) < 0 ? -1 : 1);
+		transform.Rotate(axis, angle);
 	}
-
+	
 	/*
 	void OnGUI() {
-		GUI.TextArea(new Rect(10, 10, Screen.height - 10, Screen.width - 10), debugOutput);
+		GUI.TextArea(new Rect(10, Screen.height - 60, Screen.width - 10, Screen.height - 40), debugOutput);
 	}
 	*/
 }
