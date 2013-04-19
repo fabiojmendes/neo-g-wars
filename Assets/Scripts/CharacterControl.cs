c1db5d5e9bdbe4426032d83a26731d5082bf4592
Fabio Mendes <fabiojmendes@gmail.com>
2013-04-18 21:27:51 -0300

    Animação e combo
    Saca só como ficou simples o script agora, o lance é controlar os
    estados pelo animator mesmo.
    
    Porra esses materias das animações tão foda hahaha, mil nomes errados...

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -2,20 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(CharacterController))]
 public class CharacterControl : MonoBehaviour {
 
 	public float animationSpeed = 1.5f;
 	
-	private CharacterController controller;
 	private Animator animator;
-	
-	static int atttackState = Animator.StringToHash("Base Layer.atack1");
 	
 	// Use this for initialization
 	void Start() {
 		animator = GetComponent<Animator>();
-		controller = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
@@ -23,24 +18,10 @@ public class CharacterControl : MonoBehaviour {
 		float v = Input.GetAxisRaw("Vertical");
 		float h = Input.GetAxisRaw("Horizontal");
 		
-		Vector3 direction = new Vector3(-h, 0, -v);
-		//direction.Normalize();
-		
-		// Move the controller
-		//controller.Move(direction * Time.deltaTime * animationSpeed);
-		
-		//Vector3 result = transform - direction;
 		animator.SetFloat("Speed", v);
 		animator.SetFloat("Direction", h);
 		animator.speed = animationSpeed;
 		
-
-		// ATACK
-		AnimatorStateInfo currentBaseState = animator.GetCurrentAnimatorStateInfo(0);
-		if (currentBaseState.nameHash == atttackState) {
-			animator.SetBool("Atack", false);
-		} else {
-			animator.SetBool("Atack", Input.GetButton("Fire1"));
-		}
+		animator.SetBool("Attack", Input.GetButton("Fire1"));
 	}
 }
